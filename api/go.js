298accb1bb65a4cb824fe180447d1494f4b5d78e
sylvainpoulet69-lang <sylvainpoulet69@gmail.com>
2025-10-19 10:48:03 +0200

    Update WEBAPP URL in API handler

--- api/go.js
+++ api/go.js
@@ -2,7 +2,7 @@
 export default async function handler(req, res) {
   try {
     // 👉 Ton /exec actuel (tu pourras le passer en variable d'environnement plus tard)
-    const WEBAPP = "https://script.google.com/macros/s/AKfycbxn-xUnPS71BGWbbWfN3QWDPglp2TPFkzTy4sFDvumZrbZ1FvCjlP2Uz69xKSBOALk/exec";
+    const WEBAPP = "https://script.google.com/macros/s/AKfycbw0khMb1N5UNM-QKeIb8QOTMPEyitjcrTHrwYQoeIojwQSULAZ_nt_hFyJt0JZNlphj/exec";
 
     const url = new URL(req.url, `http://${req.headers.host}`);
     const go = url.searchParams.get("go") || "";
