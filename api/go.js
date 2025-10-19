7cbf81faf2b0861d662918b62cd4e994acbf20af
sylvainpoulet69-lang <sylvainpoulet69@gmail.com>
2025-10-19 10:39:42 +0200

    Update go.js

--- api/go.js
+++ api/go.js
@@ -2,7 +2,7 @@
 export default async function handler(req, res) {
   try {
     // 👉 Ton /exec actuel (tu pourras le passer en variable d'environnement plus tard)
-    const WEBAPP = "https://script.google.com/macros/s/AKfycby-LAegFtJQDK1Xh4Zwf44Fib26eJqXDIBVT5PuaG65P8TTSwPXft3qiZCIiD--ylfb/exec";
+    const WEBAPP = "https://script.google.com/macros/s/AKfycbxn-xUnPS71BGWbbWfN3QWDPglp2TPFkzTy4sFDvumZrbZ1FvCjlP2Uz69xKSBOALk/exec";
 
     const url = new URL(req.url, `http://${req.headers.host}`);
     const go = url.searchParams.get("go") || "";
