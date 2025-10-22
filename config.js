c6a113f1508cac60372ce17224f7375a182d72ad
sylvainpoulet69-lang <sylvainpoulet69@gmail.com>
2025-10-23 00:03:23 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Hardcode ici l'URL **/exec** de ta WebApp Apps Script.
+// Exemple : "https://script.google.com/macros/s/AKfycbx.../exec"
+// ⚠️ Remplace bien la valeur ci-dessous.
+exports.WEBAPP_URL = "PASTE_YOUR_APPS_SCRIPT_EXEC_URL_HERE";
