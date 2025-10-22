984a3d4ff701decec0db65d90f2b989ea34323d1
sylvainpoulet69-lang <sylvainpoulet69@gmail.com>
2025-10-23 01:35:43 +0200

    Update pro.js

--- api/pro.js
+++ api/pro.js
@@ -1,6 +1,6 @@
 // api/pro.js — version ultra simple, aucun import
 // ⬇️ colle ici l'URL /exec exacte de ta WebApp Apps Script
-const WEBAPP_URL = "https://script.google.com/macros/s/TON_ID/exec";
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwRe_Cya9fYaIcxUNwq25UycH1eSuhtnAVT525ktsUpKQfZBhznwO8OQk9PPIKmgUoT/exec";
 
 module.exports = (req, res) => {
   try {
