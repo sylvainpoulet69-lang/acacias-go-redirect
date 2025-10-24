15a5841127894c7f4eeee91f247e7ec74ba4bced
sylvainpoulet69-lang <sylvainpoulet69@gmail.com>
2025-10-24 23:55:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Hardcode ici l'URL **/exec** de ta WebApp Apps Script.
 // Exemple : "https://script.google.com/macros/s/AKfycbx.../exec"
 // ⚠️ Remplace bien la valeur ci-dessous.
-exports.WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwwAp4qftULWJ6EHKmDhjK02L_UOUoXNL2tSZR7lksQYAjAImbsyEC6Tf2yABoRPgwu/exec";
+exports.WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyr6fues4H5sSzxxSN-bNmeDj6GUqoXjZ1HuHpOjhYfCZZKXtVj19X06Uzy5FroLgAC/exec";
