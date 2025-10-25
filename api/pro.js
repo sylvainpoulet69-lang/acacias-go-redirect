96d242354190fc258181ab3aa3337e04a5b2a9f2
sylvainpoulet69-lang <sylvainpoulet69@gmail.com>
2025-10-26 00:38:06 +0200

    Update WEBAPP_URL to new endpoint

--- api/pro.js
+++ api/pro.js
@@ -1,6 +1,6 @@
 // api/pro.js — version ultra simple, aucun import
 // ⬇️ colle ici l'URL /exec exacte de ta WebApp Apps Script
-const WEBAPP_URL = "https://script.google.com/macros/s/AKfycby6qEuVLcP6g_0PH_lBg7HnSCCQ2C22KGi4JT8OjlctWqs8S5IO5yBnI2FP01Y8c0oY/exec";
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwdzrZXbhQXXlEBl3DfNvUmSELJTSVcGucyGrwn1O3oNr8cNmDIE7mISbFzvWxLe7wf/exec";
 
 module.exports = (req, res) => {
   try {
